a566b20d8c09fb8b9802fc6801bfd462b1541e10
dogTaksa <gaisrininkas112@gmail.com>
2024-12-22 20:19:04 +0200

    Setup for Combat and Magic System
    CombatStats (hp, mp, attack, resists, etc.), abstract class Rune, rune_Shield

---
+++ Assets/CombatSystem/CombatStats.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CombatStats : MonoBehaviour
+{
+    public int HP = 100;
+    public int maxHP = 100;
+    public int hpRecoveryPerSecond;
+    public int shield = 0;
+
+    public int attack = 10;
+
+    public float MP = 10;
+    public float maxMP = 10;
+    public float mpRecoveryPerSecond;
+
+    public float physicalResist = 0;
+    public float magicalResist = 0;
+    public float buffs = 0;
+
+    public bool manualRecoveryperSecond = false;
+    float hpRestoreTimer = 0;
+    float mpRestoreTimer = 0;
+
+    private void Start()
+    {
+        if (!manualRecoveryperSecond)
+        {
+            hpRecoveryPerSecond = maxHP / 20;
+            mpRecoveryPerSecond = maxMP / 100;
+        }
+    }
+
+    private void Update()
+    {
+        while (HP != maxHP)
+        {
+            hpRestoreTimer += Time.deltaTime;
+            if (hpRestoreTimer >= 1)
+            {
+                hpRestoreTimer = 0;
+                HP += hpRecoveryPerSecond;
+            }
+        }
+
+        while (MP != maxMP)
+        {
+            mpRestoreTimer += Time.deltaTime;
+            if (mpRestoreTimer >= 1)
+            {
+                mpRestoreTimer = 0;
+                MP += mpRecoveryPerSecond;
+            }
+        }
+
+        if (HP > maxHP)
+        {
+            HP = maxHP;
+        }
+
+        if (MP > maxMP)
+        {
+            MP = maxMP;
+        }
+    }
+    public void DealDamageToThis(int damage, string damageType)
+    {
+        if (damageType == "Physical")
+        {
+            damage *= (int)(1 - physicalResist);
+        }
+        else if (damageType == "Magical")
+        {
+            damage *= (int)(1 - magicalResist);
+        }
+
+        if (shield > 0)
+        {
+            if (shield >= damage)
+            {
+                shield -= damage;
+            }
+            else
+            {
+                damage -= shield;
+                shield = 0;
+                HP -= damage;
+            }
+        }
+        else
+        {
+            HP -= damage;
+
+            if (HP < 0)
+            {
+                HP = 0;
+            }
+        }
+    }
+
+    public int CalculateDamageForOther()
+    {
+        return attack * (int)(1 + buffs);
+    }
+
+    public void RestoreHP(int healValue)
+    {
+        HP += healValue;
+
+        if (HP > maxHP)
+        {
+            HP = maxHP;
+        }
+    }
+}
