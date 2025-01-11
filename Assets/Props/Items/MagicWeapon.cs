d29dd971f2edcd0df2ebe0679584e375faea1a37
dogTaksa <gaisrininkas112@gmail.com>
2025-01-11 14:59:43 +0200

    continue for items

--- Assets/Props/Items/MagicWeapon.cs
+++ Assets/Props/Items/MagicWeapon.cs
@@ -13,8 +13,8 @@ public class MagicWeapon : MonoBehaviour
 [Serializable]
 public struct MagicWeaponStats
 {
-    public int Type; // 0: Палочки и посохи, 1: Spell book, 2: Mana Vault
-    public string Rarity;
+    public int Type; // 0: Scepters and other, 1: Spell book, 2: Mana Vault
+    public float CD;
     public List<Spell> ListOfSpells;
     public int VaultOfMana;
     public int VaultOfManaMaxSize;
