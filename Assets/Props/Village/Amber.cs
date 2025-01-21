dc25b70119e8797b82e4e275379cfe837a5e9edf
dogTaksa <gaisrininkas112@gmail.com>
2025-01-21 16:47:55 +0200

    Size for commit

---
+++ Assets/Props/Village/Amber.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amber : MonoBehaviour
+{
+    public GameObject Wall1;
+    public GameObject Wall2;
+    public GameObject Wall3;
+    public GameObject Wall4;
+
+    public Barrier SanctuaryBarrier;
+    public Barrier VillageBarrier;
+
+    public bool isNight = false;
+
+    private void Update()
+    {
+        if (isNight)
+        {
+
+        }
+    }
+}
---
+++ Assets/Props/Village/Barrier.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Material color: 898989 => DCDCDC => wait ~0.5s => 898989
+    }
+}
